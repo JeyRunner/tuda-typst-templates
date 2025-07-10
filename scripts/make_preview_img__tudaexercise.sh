8b38317635a791f4ff1daad94ebeafcb9b3a3610
JeyRunner <JeyRunner@users.noreply.github.com>
2025-07-10 20:50:08 +0200

    fix exercise preview

--- scripts/make_preview_img__tudaexercise.sh
+++ scripts/make_preview_img__tudaexercise.sh
@@ -1,1 +1,1 @@
-pdftoppm tests/tudaexercise/test_tudaexercise.pdf templates/tudaexercise/preview/tudaexercise_prev -png -l 1
+pdftoppm templates_examples/tudaexercise/main.pdf templates/tudaexercise/preview/tudaexercise_prev -png -l 1
